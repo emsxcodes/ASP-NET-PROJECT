0555ccf0712d23d2d5ca3ee3b4db3eb71de7776b
mikolajstach <120037785+emsxcodes@users.noreply.github.com>
2024-01-21 23:55:01 +0100

    Update BugFix.cs

--- Bug/Models/BugFix.cs
+++ Bug/Models/BugFix.cs
@@ -8,7 +8,9 @@ namespace Bug.Models
         [Key]
         [Column(TypeName = "int")]
         public int FixId { get; set; }
-
+        
+        [Required(ErrorMessage = "Description is required")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "Description can only contain letters, numbers, and spaces.")]
         [Column(TypeName = "nvarchar(200)")]
         public string FixDescription { get; set; }
 
